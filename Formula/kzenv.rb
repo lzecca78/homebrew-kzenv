1b259adb1ffd3e640fde908e038140a488d5e5f7
Luca Zecca <l.zecca78@gmail.com>
2020-01-12 15:16:05 +0100

    bumped release v0.4

--- Formula/kzenv.rb
+++ Formula/kzenv.rb
@@ -1,11 +1,10 @@
 class Kzenv < Formula
-  KUBENVZ_VERSION = "v0.3".freeze
+  KUBENVZ_VERSION = "v0.4".freeze
 
   desc "Installs Kzenv from github"
   homepage "https://github.com/lzecca78/kzenv"
   url "https://github.com/lzecca78/kzenv/archive/#{KUBENVZ_VERSION}.zip"
   version #{KUBENVZ_VERSION}
-  sha256 'a9e5d7cd14d64cf9ac1f2a92e5cce5c6db3d3f624ae5f01e6f661736cd48d2a9'
 
   bottle :unneeded
 
